1e648fac2e1195c9d7bf437480269748e44eff70
Brian Cravens <brian.cravens303@gmail.com>
2020-04-21 00:39:26 -0500

    Function Arg and Multi Punctuation

--- overly-excited.js
+++ overly-excited.js
@@ -6,21 +6,25 @@ let sentence = ["The","walrus","danced","through","the","trees","in","the","ligh
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
+function addExcitement (theWordArray,punctuation,multi) {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
+    let wordCount = 1;
 
     for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-        buildMeUp += " " +sentence[i]
-        // console.log (buildMeUp += [i])
-
-        // Print buildMeUp to the console
-        console.log(buildMeUp)
+        if (wordCount % 3 === 0){
+            buildMeUp += " " +theWordArray[i]+punctuation.repeat(multi)
+            wordCount++;
+        }
+        else{
+        buildMeUp += " " +theWordArray[i]
+        wordCount++;        
+        }
+        console.log(buildMeUp);
     }
 
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence)
+addExcitement(sentence, "!", 4)
